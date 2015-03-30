445f55738ea8d2d00a459551c142b75c8485ba75
Natanael Log <natte.log@gmail.com>
2015-03-30 20:51:21 +0200

    Made ILAction a MouseListener

--- src/dev/ILAction.java
+++ src/dev/ILAction.java
@@ -1,12 +1,13 @@
 package dev;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 /**
  * Created by nattelog on 15-03-19.
  */
-public class ILAction
+public class ILAction implements MouseListener
 {
     private Canvas canvas;
 
@@ -20,5 +21,46 @@ public class ILAction
 		canvas.setUserState(state);
 	    }
 	};
+    }
+
+    @Override public void mouseClicked(final MouseEvent e) {
+	System.out.println("mouseclick: (" + e.getX() + ", " + e.getY() + ")");
+
+	        int x = e.getX();
+	        int y = e.getY();
+	        Color color = Color.BLACK;
+
+	        switch (canvas.getUserState()) {
+	            case CIRCLE:
+	                int radius = 10;
+	                canvas.addShape(new Circle(x, y, radius, color));
+	                break;
+
+	            case RECTANGLE:
+	                int width = 10;
+	                int height = 10;
+	                canvas.addShape(new Rectangle(x, y, width, height, color));
+	                break;
+
+	            default:
+	                System.out.println("The current state " +  canvas.getUserState() + " is not defined in mouseClicked.");
+	                break;
+	        }
+    }
+
+    @Override public void mousePressed(final MouseEvent e) {
+
+    }
+
+    @Override public void mouseReleased(final MouseEvent e) {
+
+    }
+
+    @Override public void mouseEntered(final MouseEvent e) {
+
+    }
+
+    @Override public void mouseExited(final MouseEvent e) {
+
     }
 }
--- src/dev/ILFrame.java
+++ src/dev/ILFrame.java
@@ -2,12 +2,11 @@ package dev;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 /**
  * Created by nattelog on 15-03-15.
  */
-public class ILFrame extends JFrame implements MouseListener
+public class ILFrame extends JFrame
 {
 
     // The size of the frame
@@ -33,7 +32,7 @@ public class ILFrame extends JFrame implements MouseListener
         // Adding canvasComponent
         canvasComponent = new ILComponent(canvas);
         canvas.addCanvasListener(canvasComponent);
-        canvasComponent.addMouseListener(this);
+        canvasComponent.addMouseListener(action);
         add(canvasComponent, BorderLayout.CENTER);
 
         // Finalizing the frame
@@ -47,47 +46,5 @@ public class ILFrame extends JFrame implements MouseListener
 
     public static int getFrameHeight() {
         return FRAME_HEIGHT;
-    }
-
-    // Depending on its state, when a user clicks on the
-    // the canvas an ILAction is called
-    @Override public void mouseClicked(final MouseEvent e) {
-        System.out.println("mouseclick: (" + e.getX() + ", " + e.getY() + ")");
-        int x = e.getX();
-        int y = e.getY();
-        Color color = Color.BLACK;
-
-        switch (canvas.getUserState()) {
-            case CIRCLE:
-                int radius = 10;
-                canvas.addShape(new Circle(x, y, radius, color));
-                break;
-
-            case RECTANGLE:
-                int width = 10;
-                int height = 10;
-                canvas.addShape(new Rectangle(x, y, width, height, color));
-                break;
-
-            default:
-                System.out.println("The current state " +  canvas.getUserState() + " is not defined in mouseClicked.");
-                break;
-        }
-    }
-
-    @Override public void mousePressed(final MouseEvent e) {
-
-    }
-
-    @Override public void mouseReleased(final MouseEvent e) {
-
-    }
-
-    @Override public void mouseEntered(final MouseEvent e) {
-
-    }
-
-    @Override public void mouseExited(final MouseEvent e) {
-
     }
 }
